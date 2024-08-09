0ba9d3edf54e6fb36c633858260de0820eb11855
T199811E <rory.greenwood@iqblade.cloud>
2024-08-09 15:14:47 +0100

    added pipeline messages with error traceback being sent to the pipelines team channel

--- main.py
+++ main.py
@@ -4,17 +4,26 @@ runs both files
 from etab_main import run_etab
 from legal_main import run_legal
 from utils import pipeline_messenger
+import sys
+import traceback
 
 if __name__ == '__main__':
     try:
         run_etab()
         pipeline_messenger(
-            title='French Companies Data Transfer',
+            title='Sirene Data Transfer (Etab) Notification',
             text='Etab Pipeline has finished running',
             notification_type='pass'
         )
     except Exception as e:
-        print(e)
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback_str = traceback.format_exception(exc_type, exc_value, exc_traceback)
+        pipeline_messenger(
+            title='Sirene Data Transfer (Etab) Notification',
+            text=str(traceback_str),
+            notification_type='fail'
+        )
+
     try:
         run_legal()
         pipeline_messenger(
@@ -23,5 +32,11 @@ if __name__ == '__main__':
             notification_type='pass'
         )
     except Exception as e:
-        print(e)
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback_str = traceback.format_exception(exc_type, exc_value, exc_traceback)
+        pipeline_messenger(
+            title='Sirene Data Transfer (Legale) Notification',
+            text=str(traceback_str),
+            notification_type='fail'
+        )
 
